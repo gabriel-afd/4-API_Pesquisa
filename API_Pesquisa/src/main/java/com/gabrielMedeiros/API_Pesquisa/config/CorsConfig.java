a753066350e016794a0639588c0a36ecd1627326
Gabrielemail <gmedeiros144@gmail.com>
2025-03-30 18:33:38 -0300

    Adicionando permissão para front-end no Render na classe CorsConfig, Alterações na Docker

--- API_Pesquisa/src/main/java/com/gabrielMedeiros/API_Pesquisa/config/CorsConfig.java
+++ API_Pesquisa/src/main/java/com/gabrielMedeiros/API_Pesquisa/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081", "https://front-api-pesquisa.onrender.com")
+                        .allowedOrigins("*") //http://localhost:5173", "http://localhost:8081
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
